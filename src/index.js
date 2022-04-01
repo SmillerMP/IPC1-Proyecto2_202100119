4c5d5284a1e7f46e83c07255c56182e314012b19
SmillerMP <samuelmp7@gmail.com>
2022-04-01 14:37:22 -0600

    Login
    Se añadió el Login del Usuario comprobando el User y la Contraseña, desde el archivo usuarios.json

--- src/index.js
+++ src/index.js
@@ -20,36 +20,76 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 
-
-
 // Rutas
 app.get('/', (req, res) => {
     res.send("Bienvenido a la pantalla principal Mother Fucker")
-})
+});
 
 
 app.get('/home', (req, res) => {
     var a = 2;
     res.send('Bienvenido a mi primera API');
-})
+});
 
 // Ruta de la pokedex
-app.get('/pokedex', (req, res) => {
-    res.send(pokedex);
-})
+app.post('/pokedex', (req, res) => {
+    var Found = true;
+    var TextoBusqueda = req.body.TextoBusqueda;
+    var OpcionesBusqueda = req.body.OpcionesBusqueda;
+
+  
+
+    if(((TextoBusqueda == "") && (OpcionesBusqueda == "")) || OpcionesBusqueda == "Todos"){
+        res.send(pokedex);
+
+    } else if (OpcionesBusqueda == "Numero"){
+        for (x of pokedex){
+            if(x.Numero == TextoBusqueda ){
+                res.send(x);
+
+                Found = true;
+                break;
+    
+            } else {
+                Found = false;
+            }
+    
+        };
+    
+        if(Found == false){
+            res.send("No se ha encontrado ningun pokemon con este numero");
+        }
+    }
+
+
+    
+});
 
 // Login de los usuarios
 app.post('/Login', (req, res) => {
-    var dato1 = req.body.dato1;
-    var dato2 = req.body.dato2;
-    var suma = dato1 + dato2;
-    var respuesta = {
-        "titulo":"Operacion",
-        "resultado":suma
-    }
-    res.send(respuesta);
+    var Found = true;
     
-})
+    var LoginUsuario = req.body.LoginUsuario;
+    var LoginPassword = req.body.LoginPassword;
+
+    for (x of Usuarios){
+        if((x.User == LoginUsuario ) && (x.Password == LoginPassword)){
+            res.send("Bienvenido Usuario " + LoginUsuario);
+            Found = true;
+            break;
+
+        } else {
+            Found = false;
+        }
+
+    };
+
+    if(Found == false){
+        res.send("Las credenciales son incorrectas");
+    }
+
+    
+});
 
 
 // Enviando en que puerto salen los datas
