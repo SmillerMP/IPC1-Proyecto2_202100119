7e0ff752a56780f258b5c6d260e35bf9e7714ca4
SmillerMP <samuelmp7@gmail.com>
2022-04-01 22:02:54 -0600

    Busqueda de Pokemons
    Se añadió la busqueda de pokemons, por Numero, Nombre y Tipo

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ const pokedex = require('./pokemons.json');
 // Llama la base de datos de Usuarios
 const Usuarios = require('./usuarios.json');
 
+// Archivo Json Temporal para la busqeuda de pokemons
+const PokeTemporal = require('./pokeTemp.json')
+
+// Pone en mayuscula la primera letra de la palabra
+function capitalize(palabra) {
+    return palabra[0].toUpperCase() + palabra.slice(1);
+};
+
+
 // Creacion de la API
 const app = express();
 
@@ -31,6 +40,7 @@ app.get('/home', (req, res) => {
     res.send('Bienvenido a mi primera API');
 });
 
+
 // Ruta de la pokedex
 app.post('/pokedex', (req, res) => {
     var Found = true;
@@ -38,29 +48,63 @@ app.post('/pokedex', (req, res) => {
     var OpcionesBusqueda = req.body.OpcionesBusqueda;
 
   
-
-    if(((TextoBusqueda == "") && (OpcionesBusqueda == "")) || OpcionesBusqueda == "Todos"){
+    // Muestra los datos completos de la Pokedex
+    if(((TextoBusqueda == "") && (OpcionesBusqueda == "")) || capitalize(OpcionesBusqueda) == "Todos"){
         res.send(pokedex);
 
-    } else if (OpcionesBusqueda == "Numero"){
+    // Busqueda de pokemos por numero
+    } else if (capitalize(OpcionesBusqueda) == "Numero"){
+        Found = false;
         for (x of pokedex){
             if(x.Numero == TextoBusqueda ){
                 res.send(x);
 
                 Found = true;
                 break;
-    
-            } else {
-                Found = false;
             }
-    
-        };
+        }
     
         if(Found == false){
             res.send("No se ha encontrado ningun pokemon con este numero");
         }
-    }
 
+    // Busqueda de pokemons por su nombre
+    } else if (capitalize(OpcionesBusqueda) == "Nombre"){
+        Found = false;
+        for (x of pokedex){
+            if(x.Nombre == capitalize(TextoBusqueda) ){
+                res.send(x);
+
+                Found = true;
+                break;
+            }   
+        }
+        if(Found == false){
+            res.send("No se ha encontrado ningun pokemon con este numero");
+        }
+
+    
+    // Busqueda de Pokemons por su tipo
+    } else if (capitalize(OpcionesBusqueda) == "Tipo"){
+
+        Found = false;
+        PokeTemporal.splice();
+        for (x of pokedex){
+            if(x.Tipo == capitalize(TextoBusqueda) ){
+                
+                // Guarda todos los pokemons del mismo tipo en un json temporal
+                PokeTemporal.push(x);
+                Found = true;
+            } 
+        }
+        if(Found == false){
+            res.send("No se ha encontrado ningun pokemon con este Tipo");
+
+        }else{
+            res.send(PokeTemporal);
+        }
+
+    }
 
     
 });
